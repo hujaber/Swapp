903b81401c3250a86f7f98d751e85f4dc9fe47fa
Hussein Jaber <hujaber@me.com>
2019-09-17 16:07:02 +0300

    remove iOS 13 code

---
+++ Swapp/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Swapp
+//
+//  Created by Hussein Jaber on 17/9/19.
+//  Copyright © 2019 Hussein Jaber. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
